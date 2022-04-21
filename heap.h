0ae09166e2d9526677967407bb986bb6eea38e71
ansicheng <ansicheng@163.com>
2022-04-22 00:07:03 +0800

    索引堆双向索引

--- heap.h
+++ heap.h
@@ -14,12 +14,12 @@
 /* 堆用数组存储，根节点在下标0开始 */
 
 template<typename T>
-void __shiftUp1(vector<T> &heap, int k)
+bool __shiftUp1(vector<T> &heap, int k)
 {
     int p = (k - 1) / 2;
     T kv = heap[k];
     if(greaterEqualHeap(heap[p], kv))
-        return;
+        return false;
     heap[k] = heap[p];
     k = p;
     p = (k - 1) / 2;
@@ -30,19 +30,20 @@ void __shiftUp1(vector<T> &heap, int k)
         p = (k - 1) / 2;
     }
     heap[k] = kv;
+    return true;
 }
 
 template<typename T>
-void __shiftDown1(vector<T> &heap, int k)
+bool __shiftDown1(vector<T> &heap, int k)
 {
     int c = k * 2 + 1;
     T kv = heap[k];
     if(c >= heap.size())
-        return;
+        return false;
     if(c + 1 < heap.size() && greaterHeap(heap[c + 1], heap[c]))
         c++;
     if(lessEqualHeap(heap[c], kv))
-        return;
+        return false;
     heap[k] = heap[c];
     k = c;
     c = k * 2 + 1;
@@ -57,6 +58,7 @@ void __shiftDown1(vector<T> &heap, int k)
         c = k * 2 + 1;
     }
     heap[k] = kv;
+    return true;
 }
 
 template<typename T>
@@ -107,12 +109,12 @@ void heapSort1(vector <T> &heap)
 /* 堆用数组存储，根节点在下标0开始 */
 
 template<typename T>
-void __shiftUp2(vector<T> &heap, int k)
+bool __shiftUp2(vector<T> &heap, int k)
 {
     int p = (k - 1) / 2;
     T kv = heap[k];
     if(lessEqualHeap(heap[p], kv))
-        return;
+        return false;
     heap[k] = heap[p];
     k = p;
     p = (k - 1) / 2;
@@ -123,19 +125,20 @@ void __shiftUp2(vector<T> &heap, int k)
         p = (k - 1) / 2;
     }
     heap[k] = kv;
+    return true;
 }
 
 template<typename T>
-void __shiftDown2(vector<T> &heap, int k)
+bool __shiftDown2(vector<T> &heap, int k)
 {
     int c = k * 2 + 1;
     T kv = heap[k];
     if(c >= heap.size())
-        return;
+        return false;
     if(c + 1 < heap.size() && lessHeap(heap[c + 1], heap[c]))
         c++;
     if(greaterEqualHeap(heap[c], kv))
-        return;
+        return false;
     heap[k] = heap[c];
     k = c;
     c = k * 2 + 1;
@@ -150,6 +153,7 @@ void __shiftDown2(vector<T> &heap, int k)
         c = k * 2 + 1;
     }
     heap[k] = kv;
+    return true;
 }
 
 template<typename T>
@@ -200,36 +204,41 @@ void heapSort2(vector <T> &heap)
 /* 堆用索引表示数组存储，根节点在下标0开始 */
 
 template<typename T>
-void __shiftUpIndex1(vector<T> &array, vector<int> &heap, int k)
+bool __shiftUp3(vector<T> &array, vector<int> &heap, vector<int> &heappos, int k)
 {
     int p = (k - 1) / 2;
     int kv = heap[k];
     if(greaterEqualHeap(array[heap[p]], array[kv]))
-        return;
+        return false;
     heap[k] = heap[p];
+    heappos[heap[k]] = k;
     k = p;
     p = (k - 1) / 2;
     while(k > 0 && lessHeap(array[heap[p]], array[kv]))
     {
         heap[k] = heap[p];
+        heappos[heap[k]] = k;
         k = p;
         p = (k - 1) / 2;
     }
     heap[k] = kv;
+    heappos[kv] = k;
+    return true;
 }
 
 template<typename T>
-void __shiftDownIndex1(vector<T> &array, vector<int> &heap, int k)
+bool __shiftDown3(vector<T> &array, vector<int> &heap, vector<int> &heappos, int k)
 {
     int c = k * 2 + 1;
     int kv = heap[k];
     if(c >= heap.size())
-        return;
+        return false;
     if(c + 1 < heap.size() && greaterHeap(array[heap[c + 1]], array[heap[c]]))
         c++;
     if(lessEqualHeap(array[heap[c]], array[kv]))
-        return;
+        return false;
     heap[k] = heap[c];
+    heappos[heap[k]] = k;
     k = c;
     c = k * 2 + 1;
     while(c < heap.size())
@@ -239,50 +248,55 @@ void __shiftDownIndex1(vector<T> &array, vector<int> &heap, int k)
         if(lessEqualHeap(array[heap[c]], array[kv]))
             break;
         heap[k] = heap[c];
+        heappos[heap[k]] = k;
         k = c;
         c = k * 2 + 1;
     }
     heap[k] = kv;
+    heappos[kv] = k;
+    return true;
 }
 
 template<typename T>
 //堆插入元素，在最后位置调用shiftUp
-void insertIndexHeap1(vector<T> &array, vector<int> &heap, T i)
+void insertHeap3(vector<T> &array, vector<int> &heap, vector<int> &heappos, T i)
 {
     array.push_back(i);
     heap.push_back(array.size() - 1);
-    __shiftUpIndex1(array, heap, heap.size() - 1);
+    __shiftUp3(array, heap, heappos, heap.size() - 1);
 }
 
 template<typename T>
 //建立堆，在第一个非叶子结点开始依次调用shiftDown
-void heapifyIndex1(vector<T> &array, vector<int> &heap)
+void heapify3(vector<T> &array, vector<int> &heap, vector<int> &heappos)
 {
     for(int i = (heap.size() - 1 - 1) / 2; i >= 0; i--)
-        __shiftDownIndex1(array, heap, i);
+        __shiftDown3(array, heap, heappos, i);
 }
 
 template<typename T>
 //取出最大元素，保持完全二叉树最后一个元素放在根调用shiftDown保持最大堆性质
-T extractMaxIndex1(vector<T> &array, vector<int> &heap)
+T extractMax3(vector<T> &array, vector<int> &heap, vector<int> &heappos)
 {
     T ret = array[heap[0]];
     heap[0] = heap[heap.size() - 1];
+    heappos[heap[0]] = 0;
     heap.pop_back();
-    __shiftDownIndex1(array, heap, 0);
+    __shiftDown3(array, heap, heappos, 0);
     return ret;
 }
 
 template<typename T>
 //堆排序，利用堆顶点最大性质，依次取出顶点与最后元素交换调用shiftDown，在原空间实现排序
-void heapSortIndex1(vector<T> &array, vector<int> &heap)
+void heapSort3(vector<T> &array, vector<int> &heap, vector<int> &heappos)
 {
-    heapifyIndex1(array, heap);
+    heapify3(array, heap, heappos);
     for(int i = heap.size() - 1; i > 0; i--)
     {
         swap(heap[0], heap[i]);
+        heappos[heap[0]] = 0;
         if(i > 1)
-            __shiftDownIndex1(array, heap, 0);
+            __shiftDownIndex1(array, heap, heappos, 0);
     }
 }
 
@@ -294,36 +308,41 @@ void heapSortIndex1(vector<T> &array, vector<int> &heap)
 /* 堆用数组存储，根节点在下标0开始 */
 
 template<typename T>
-void __shiftUpIndex2(vector<T> &array, vector<int> &heap, int k)
+bool __shiftUp4(vector<T> &array, vector<int> &heap, vector<int> &heappos, int k)
 {
     int p = (k - 1) / 2;
     int kv = heap[k];
     if(lessEqualHeap(array[heap[p]], array[kv]))
-        return;
+        return false;
     heap[k] = heap[p];
+    heappos[heap[k]] = k;
     k = p;
     p = (k - 1) / 2;
     while(k > 0 && greaterHeap(array[heap[p]], array[kv]))
     {
         heap[k] = heap[p];
+        heappos[heap[k]] = k;
         k = p;
         p = (k - 1) / 2;
     }
     heap[k] = kv;
+    heappos[kv] = k;
+    return true;
 }
 
 template<typename T>
-void __shiftDownIndex2(vector<T> &array, vector<int> &heap, int k)
+void __shiftDown4(vector<T> &array, vector<int> &heap, vector<int> &heappos, int k)
 {
     int c = k * 2 + 1;
     int kv = heap[k];
     if(c >= heap.size())
-        return;
+        return false;
     if(c + 1 < heap.size() && lessHeap(array[heap[c + 1]], array[heap[c]]))
         c++;
     if(greaterEqualHeap(array[heap[c]], array[kv]))
-        return;
+        return false;
     heap[k] = heap[c];
+    heappos[heap[k]] = k;
     k = c;
     c = k * 2 + 1;
     while(c < heap.size())
@@ -333,49 +352,54 @@ void __shiftDownIndex2(vector<T> &array, vector<int> &heap, int k)
         if(greaterEqualHeap(array[heap[c]], array[kv]))
             break;
         heap[k] = heap[c];
+        heappos[heap[k]] = k;
         k = c;
         c = k * 2 + 1;
     }
     heap[k] = kv;
+    heappos[kv] = k;
+    return true;
 }
 
 template<typename T>
 //堆插入元素，在最后位置调用shiftUp
-void insertHeapIndex2(vector<T> &array, vector<int> &heap, T i)
+void insertHeap4(vector<T> &array, vector<int> &heap, vector<int> &heappos, T i)
 {
     heap.push_back(i);
-    __shiftUpIndex2(array, heap, heap.size() - 1);
+    __shiftUp4(array, heap, heappos, heap.size() - 1);
 }
 
 template<typename T>
 //建立堆，在第一个非叶子结点开始依次调用shiftDown
-void heapifyIndex2(vector<T> &array, vector<int> &heap)
+void heapify4(vector<T> &array, vector<int> &heap, vector<int> &heappos)
 {
     for(int i = (heap.size() - 1 - 1) / 2; i >= 0; i--)
-        __shiftDownIndex2(array, heap, i);
+        __shiftDown4(array, heap, heappos, i);
 }
 
 template<typename T>
 //取出最小元素，保持完全二叉树最后一个元素放在根调用shiftDown保持最大堆性质
-T extractMinIndex2(vector<T> &array, vector<int> &heap)
+T extractMinIndex2(vector<T> &array, vector<int> &heap, vector<int> &heappos)
 {
     T ret = array[heap[0]];
     heap[0] = heap[heap.size() - 1];
+    heappos[heap[0]] = 0;
     heap.pop_back();
-    __shiftDownIndex2(array, heap, 0);
+    __shiftDown4(array, heap, heappos, 0);
     return ret;
 }
 
 template<typename T>
 //堆排序，利用堆顶点最小性质，依次取出顶点与最后元素交换调用shiftDown，在原空间实现排序
-void heapSortIndex2(vector<T> &array, vector<int> &heap)
+void heapSort4(vector<T> &array, vector<int> &heap, vector<int> &heappos)
 {
-    heapifyIndex2(array, heap);
+    heapify4(array, heap, heappos);
     for(int i = heap.size() - 1; i > 0; i--)
     {
         swap(heap[0], heap[i]);
+        heappos[heap[0]] = 0;
         if(i > 1)
-            __shiftDownIndex2(array, heap, 0);
+            __shiftDown4(array, heap, heappos, 0);
     }
 }
 
