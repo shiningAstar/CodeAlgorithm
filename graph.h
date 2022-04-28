02e932a127c0136e17b52bd6d57e84539683e1ee
ansicheng <ansicheng@163.com>
2022-04-28 16:25:36 +0800

    有向图是否存在环

--- graph.h
+++ graph.h
@@ -157,4 +157,49 @@ void bfsGraph(vector<vector<int>> &adj)
 
 /*******************************/
 
+/*******************************/
+//递归实现有向图邻接表深度优先遍历，判断是否有环
+
+//visited记录节点是否遍历，三种状态：未遍历、在遍历中（邻接节点未遍历完）、结束遍历（邻接节点全部遍历）{-1，0，1}
+//遍历中的节点被再次遍历到，则存在环
+/* 结束的节点后面节点全部遍历完不可能是当前遍历中路径的前面节点，遍历中节点一定在当前遍历中路径前面的某位置 */
+bool __dfs(vector<vector<int>> &adj, vector<int> &visited, int v)
+{
+    //遍历v
+    //cout<<nodes[v].val;
+    visited[v] = 0;
+
+    for(int i = 0; i < adj[v].size(); i++)
+    {
+        int w = adj[v][i];
+
+        if(visited[w] == 0)
+            return false;
+
+        if(visited[w] != 1 && !__dfs(adj, visited, w))
+            return false;
+    }
+    visited[v] = 1;
+    return true;
+}
+
+bool dfsGraph(vector<vector<int>> &adj)
+{
+    vector<int> visited(adj.size(), -1);
+
+    //连通图只遍历一遍，不用记录ccount和id
+    //return __dfs(adj, visited, 0);
+
+
+    for(int i = 0; i < adj.size(); i++)
+    {
+        if(visited[i] < 1 && !__dfs(adj, visited, i))
+            return false;
+    }
+    return true;
+
+}
+
+/*******************************/
+
 #endif // GRAPH_H_INCLUDED
